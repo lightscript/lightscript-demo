bce2c2694999b7f35c921baa5c754b40cfc074f6
Alex Rattray <rattray.alex@gmail.com>
2016-11-24 15:11:42 -0800

    Updated examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
 for i from 0 til 10 {
   console.log(i)
 }
 
 
-for i from 5 til 10 step 2 {
+for i from 5 + 4 til 20 {
   console.log(i)
 }
-
-
-for i from 5 + 4 til (i === 20) step (i *= 2) {
-  console.log(i)
-}
-
 
 arr := [3, 4, 5]
 
@@ -22,4 +21,16 @@ for i from arr {
 
 for i, x from arr {
   console.log(i, x)
+}
+
+for let i = 0; i < 10; i++ {
+  console.log(i);
+}
+
+for x of arr {
+  console.log(x);
+}
+
+for k in arr {
+  console.log(k);
 }
